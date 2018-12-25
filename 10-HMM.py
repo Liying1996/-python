9321b4d595ed92966db92f3b735b91e41a13a70b
Liying <ly20134626@gmail.com>
2018-12-25 11:19:00 +0800

    Update 10-HMM.py

--- 10-HMM.py
+++ 10-HMM.py
@@ -10,15 +10,15 @@ class HMM:
 	def forward(self):
 		self.all_pro = array(zeros((len(self.state), len(self.trans))))
 
-		# when t = 1:
+		# when i = 1:
 		self.all_pro[0] = self.primary * self.emis[:,self.state[0]] 
 
-		# From t = 2 until end
+		# From i = 2 until end
 		for j in range(1, len(self.state)):
 			self.all_pro[j] = (self.all_pro[j - 1] * self.trans.T).sum(axis = 1) * self.emis[:, self.state[j]]
 
 		# for k in range(len(x)):
-		# 	print("when t = {} : ".format(k))
-		# 	print(x[k])
+		# 	print("when i = {} : ".format(k))
+		# 	print(self.all_pro[k])
 
 		return "The final Probability is : {}".format(sum(self.all_pro[-1]))
