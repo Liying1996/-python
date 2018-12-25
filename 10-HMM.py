2d3915ee9c2055df568a067d56cedf4e33d6b536
Liying <ly20134626@gmail.com>
2018-12-25 14:00:46 +0800

    Update 10-HMM.py

--- 10-HMM.py
+++ 10-HMM.py
@@ -8,15 +8,15 @@ class HMM:
 	
     
 	def forward(self):
-		self.all_pro = array(zeros((len(self.state), len(self.trans))))
+		self.all_for = array(zeros((len(self.state), len(self.trans))))
 
 		# when i = 1:
-		self.all_pro[0] = self.primary * self.emis[:,self.state[0]] 
+		self.all_for[0] = self.primary * self.emis[:,self.state[0]] 
 
 		# From i = 2 until end
 		for j in range(1, len(self.state)):
-			self.all_pro[j] = (self.all_pro[j - 1] * self.trans.T).sum(axis = 1) * self.emis[:, self.state[j]]
+			self.all_for[j] = (self.all_for[j - 1] * self.trans.T).sum(axis = 1) * self.emis[:, self.state[j]]
 
-		# print(self.all_pro)
+		# print(self.all_for)
 
-		return "The final Probability is : {}".format(sum(self.all_pro[-1]))
+		return "The final Probability is : {}".format(sum(self.all_for[-1]))
